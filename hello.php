0a567d650e3ab8d8ba32b0fbb3603f9657717a49
Daniel Zamanillo <dzamanillok@gmail.com>
2021-12-10 15:42:06 -0600

    done

---
+++ hello.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Hello World Script</title>
+    </head>
+    <body>
+        <p><?php echo 'Hello World'; ?></p>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Best practice -->
+<?php 
+    echo 'Hello World';
+?>
+
+<!-- Not recommended -->
+<?='Hello World'?>
+
+<!-- Echo is faster -->
+<?php
+    print 'Hello World';
+?>
